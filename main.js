fc260fdbbdf7932c935c0d1066ee03aef92aec05
ollie <oliver.a.hogue@gmail.com>
2021-04-06 20:59:02 +1000

    wrote testable main.js

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ import API from './api.js';
 const api = new API('https://ollieiswoke.github.io/favourite-things/');
 
 //makes get request to the website...
-//api.makeAPIRequest('upload', "", 'GET', '?id=david_monroe', '')
+api.makeAPIRequest('upload', "", 'GET', '?id=david_monroe', '')
+
+//const response = await fetch("upload", {
+//   method: 'GET',
+//    });
